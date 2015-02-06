ee9725904162dee977a10d6b566ccdd774b497f0
Colin Hutchinson <chutchinson@freshbooks.com>
2015-02-06 13:42:10 -0500

    ENV variable for root password

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 liquibase --driver=com.mysql.jdbc.Driver \
      --changeLogFile=/changelogs/migrations.xml \
-     --url=jdbc:mysql://${MYSQL_PORT_3306_TCP_ADDR}:$MYSQL_PORT_3306_TCP_PORT/asm \
+     --url=jdbc:mysql://${MYSQL_PORT_3306_TCP_ADDR}:${MYSQL_PORT_3306_TCP_PORT}/asm \
      --username=root \
-     --password= \
+     --password=${MYSQL_ENV_MYSQL_ROOT_PASSWORD} \
      update
